e3de86204a4a0d7970116dd8f3ee1ff1af7b14b5
Maxiboii <67202298+Maxiboii@users.noreply.github.com>
2020-09-06 21:20:52 +0300

    Working for one testform

---
+++ formbot.py
@@ -0,0 +1,58 @@
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+class Google:
+
+    def __init__(self,username,password):
+        self.driver=webdriver.Chrome('/Users/Max/Desktop/Code/Selenium/chromedriver')
+        #self.driver.get('https://stackoverflow.com/users/login') #stack
+        self.driver.get('https://www.coursera.org/?authMode=login') #coursera
+        original_window = self.driver.current_window_handle #coursera
+        sleep(5)
+        #self.driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click() #stack
+        self.driver.find_element_by_xpath('//*[@id="authentication-box-content"]/div/div[1]/div[1]/button/span').click() #coursera
+        sleep(5)
+        for window_handle in self.driver.window_handles: #coursera
+            if window_handle != original_window: #coursera
+                self.driver.switch_to.window(window_handle) #coursera
+                break  #coursera
+        self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(username)
+        #self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click() #stack
+        sleep(5)
+        self.driver.find_element_by_xpath('//input[@type="password"]').send_keys(password)
+        # self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click() #stack
+        sleep(5)
+        self.driver.switch_to.window(original_window) #coursera
+        self.driver.get('https://docs.google.com/forms/d/e/1FAIpQLScGJSvv9BSk-noulXix0JJrkkUxmmIoN5oArxWEgWPxOIcXcg/viewform?vc=0&c=0&w=1&flr=0&usp=mail_form_link')
+        self.next_button = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div[2]'
+        sleep(3)
+        self.driver.find_element_by_xpath('//*[@id="i5"]/div[3]/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div').click()
+        sleep(2)
+        self.driver.find_element_by_xpath('//*[@id="i11"]/div[3]/div').click()
+        self.driver.find_element_by_xpath(self.next_button).click()
+        sleep(2)
+        self.driver.find_element_by_xpath('//*[@id="i8"]/div[3]/div').click()
+        self.driver.find_element_by_xpath(self.next_button).click()
+        sleep(5)
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[2]/label[2]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[4]/label[2]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[8]/label[2]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[8]/label[3]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[10]/label[2]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[12]/label[2]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[6]/label[5]/div/div').click()
+        self.driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[6]/label[8]/div/div').click()
+
+        self.driver.find_element_by_xpath('//*[@id="i19"]/div[3]/div').click()
+
+
+
+passw = open('passw.txt',"r",encoding="utf-8")
+password=str(passw.read())
+user=open('user.txt',"r",encoding="utf-8")
+username=str(user.read())
+mylike = Google(username,password)
